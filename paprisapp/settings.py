e81024b55096d7e632ee214c3a6846fb5185a6ad
alx <alx@devdeb.mycode.mx>
2015-06-03 12:32:20 -0600

    fix release

--- paprisapp/settings.py
+++ paprisapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r4d&g=_i&831j7(rpn(5u_b=(6dtmnp(01%2j4v+kfhtyz=8zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
